5d25255e865d6c6036b0675ff2db62330fa624c5
Andreas <andreas.nastevski@hotmail.com>
2021-04-12 12:50:26 +0200

    Working on music implementation

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const PREFIX = '$';
+const ytdl = require("ytdl-core");
+
+const PREFIX = '!';
+
+var Version = 'Version 1.0.0';
+
+var Ping = 'hva skjer med faren din mann';
+
+var Website = 'https://breakingbad.fandom.com/wiki/Hector_Salamanca';
+
+var InvalidARG = 'Invalid Argument';
+
+var Servers = {};
+
 //konsollen sier at boten er klar
 //til bruk på serveren.
 client.once('ready', () => {
@@ -14,21 +27,63 @@ client.on('message', message=>{
 
     switch(args[0]){
         case 'ping':
-            message.channel.send('hva skjer med faren din mann')
+            message.channel.send(Ping)
             break;
         case 'website':
-            message.channel.send('https://breakingbad.fandom.com/wiki/Hector_Salamanca')
+            message.channel.send(Website)
             break;
         case 'info':
             if(args[1] === 'version'){
-                message.channel.send('Version 1.0.0')
+                message.channel.send(Version)
             }else{
-                message.channel.send('Invalid Argument')               
+                message.channel.send(InvalidARG)               
             }
             break;
-    }
+        case 'clear':
+            if(!args[1]) return message.reply('Error please define second argument')
+            message.channel.bulkDelete(args[1]);
+            break;
+        case 'play':
+
+            function play(connection, message){ 
+                var server = servers[message.guild.id];
+                
+                server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+                
+                server.queue.shift();
+                
+                server.dispatcher.on("end", function(){
+                    if(server.queue[0]){
+                        play(connection, message);
+                    }else {
+                        connection.disconnect();
+                    }
+                })
+            }
+
+
+            if(!args[1]){
+                message.channel.send("Please provide a link!");
+                return;
+            }
+            if(!message.member.voiceChannel){
+                message.channel.send("You must be in a voice channel!");
+                return;
+            }
+            if(!Servers[message.guild.id]) Servers[message.guild.id] = {
+                queue: []
+            }
+            var server = servers[message.guild.id];
+
+            server.queue.push(args[1]);
+
+            if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
+                play(connection, message);
+            }) 
+            break;
+    }   
 })
 
 
 
-client.login('ODMwMDU0MDQ2Mzc0NDI4Njgy.YHBGEg.YZQwMaUdteF05TYWlurD0GFk0Tw')
+client.login('ODMwMDU0MDQ2Mzc0NDI4Njgy.YHBGEg.dGMQbIuxPJgNFtoYiHTFG7jeldw')
